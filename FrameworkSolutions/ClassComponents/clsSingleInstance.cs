abc79d95ef5f58e4b0b7a421d0dde19b745208fb
John Henry Joromat <jhenryj09@gmail.com>
2022-06-02 09:14:49 +0800

    Framework Solutions

---
+++ FrameworkSolutions/ClassComponents/clsSingleInstance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace FrameworkSolutions.ClassComponents
+{
+    public class clsSingleInstance
+    {
+        private const int SW_RESTORE = 9;
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+    }
+}
