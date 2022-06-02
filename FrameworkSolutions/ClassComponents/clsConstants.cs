c884ae3dc00f0dc049e8dd3c3214238f5c047101
John Henry Joromat <john.joromat@fxvps.biz>
2022-06-02 08:32:01 +0800

    first commit

---
+++ FrameworkSolutions/ClassComponents/clsConstants.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkSolutions.ClassComponents
+{
+    public class clsConstants
+    {
+        public const string LOAD_WEBSITE = "jhnhnryjrmt.github.io";
+        public const string MAINTAIN_STATUS_ACTIVE = "ACTIVE";
+        public const string MAINTAIN_STATUS_INACTIVE = "IN-ACTIVE";
+        public const string YES = "YES";
+        public const string NO = "NO";
+        public static bool NagFlag;
+        public static string USERNAME { get; set; }
+
+        public static string ACCESS { get; set; }
+
+        public enum Form_Mode
+        {
+            Add,
+            Update,
+            ReadOnly
+        }
+    }
+}
